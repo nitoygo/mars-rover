6f7fc420b7671b1f0622389c4db16a804d724974
Benito M. Go III <benitogothethird@hotmail.com>
2024-02-04 03:38:16 +0800

    Added initial unit tests

---
+++ src/test/java/org/nasa/marsrover/rover/core/application/ManeuverServiceTest.java
@@ -0,0 +1,83 @@
+package org.nasa.marsrover.rover.core.application;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.nasa.marsrover.rover.core.ports.in.types.ManeuverCommand;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ManeuverServiceTest {
+
+    @Mock
+    private Rover rover;
+
+    @InjectMocks
+    private ManeuverService maneuverService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void maneuverShouldMoveRoverForward() {
+        maneuverService.maneuver(new ManeuverCommand("M"));
+        verify(rover, times(1)).moveForward();
+    }
+
+    @Test
+    void maneuverShouldFaceRoverLeft() {
+        maneuverService.maneuver(new ManeuverCommand("L"));
+        verify(rover, times(1)).faceLeft();
+    }
+
+    @Test
+    void maneuverShouldFaceRoverRight() {
+        maneuverService.maneuver(new ManeuverCommand("R"));
+        verify(rover, times(1)).faceRight();
+    }
+
+    @Test
+    void maneuverShouldHandleEmptyInstruction() {
+        maneuverService.maneuver(new ManeuverCommand(""));
+        // No interactions with rover should occur for empty instruction
+        verifyNoInteractions(rover);
+    }
+
+    @Test
+    void maneuverShouldHandleMultipleMoves() {
+        maneuverService.maneuver(new ManeuverCommand("MLRM"));
+        // Verify the expected sequence of interactions
+        inOrder(rover).verify(rover).moveForward();
+        inOrder(rover).verify(rover).faceLeft();
+        inOrder(rover).verify(rover).faceRight();
+        inOrder(rover).verify(rover).moveForward();
+    }
+
+    @Test
+    void maneuverShouldRejectNullInstruction() {
+        assertThrows(NullPointerException.class, () -> maneuverService.maneuver(new ManeuverCommand(null)));
+        //assertThrows(IllegalArgumentException.class, () -> maneuverService.maneuver(new ManeuverCommand(null)));
+        verifyNoInteractions(rover);
+    }
+
+    @Test
+    void maneuverShouldRejectInvalidMoves() {
+        assertThrows(IllegalArgumentException.class, () ->
+                maneuverService.maneuver(
+                        new ManeuverCommand("XYZ")));
+
+        // No interactions with rover should occur for invalid moves
+        verifyNoInteractions(rover);
+    }
+
+}
